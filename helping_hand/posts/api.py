4ee65a9ca0b62dac651b6de655cc8e9cf743842a
Tomy <tomyleereyes@gmail.com>
2019-02-02 17:43:23 -0800

    initial django rest set-up

---
+++ helping_hand/posts/api.py
@@ -0,0 +1,16 @@
+from posts.models import Post
+from rest_framework import viewsets, permissions
+from .serializers import PostSerializer
+
+# Post ViewSet
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    permission_classes = [
+
+        permissions.AllowAny
+
+    ]
+
+    serializer_class = PostSerializer
